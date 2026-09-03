e5ad33fd7461ae54a33fbbbed7ff8797cefba430
thilomoi05 <thiloth@uia.no>
2026-09-03 11:53:49 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Beredskapsportal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
